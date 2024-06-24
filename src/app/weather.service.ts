aeb67fd20875bba50a05a94099715ab52fb0607a
ens-csv <enrique.navarro64@gmail.com>
2024-06-23 23:53:57 -0600

    OpenWeather API

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Weather } from './weather';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+}
+
+private apiUrl = 'https://api.openweathermap.org/data/2.5/';
+private apiKey = '34a20c604c8aa94e8fd5224b8628e00a';
+
+getWeather(city: string): Observable<Weather> {
+    const options = new HttpParams()
+        .set('units', 'metric')
+        .set('q', city)
+        .set('appId', this.apiKey);
+    return this.http.get<Weather>(this.apiUrl + 'weather', { params:
+options });
 }
